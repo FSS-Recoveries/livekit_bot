504e544eff19ad11040bb7f3c0a978b0cc033b10
OlaOye24 <light.omi1@gmail.com>
2026-08-10 23:29:08 +0100

    20260810 - using fish tts

--- va_bot.py
+++ va_bot.py
@@ -706,8 +706,8 @@ async def entrypoint(ctx: JobContext):
         [
             inference.TTS(
                 model="fishaudio/s2-pro",
-                voice="v_ebJJAf8QhLMs",#"v_tkbNkcSD62zN",#"",
-                extra_kwargs={"speed": 1.2, "temperature": 0, "latency": "low"},
+                voice="v_tkbNkcSD62zN",#"v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
+                extra_kwargs={"speed": 1.1, "temperature": 0, "latency": "low"},
             ),
             #build_azure_tts(),
             #build_elevenlabs_tts(),
@@ -822,7 +822,7 @@ async def entrypoint(ctx: JobContext):
     # here since this only plays into the outbound track, but worth knowing
     # if it starts sounding intrusive).
     background_audio = BackgroundAudioPlayer(
-        ambient_sound=AudioConfig(BuiltinAudioClip.OFFICE_AMBIENCE, volume=0.8),
+        ambient_sound=AudioConfig(BuiltinAudioClip.OFFICE_AMBIENCE, volume=0.7),
     )
     await background_audio.start(room=ctx.room, agent_session=session)
 
