3e494f5a7feec60487f197a7d29879b9e2e2b015
OlaOye24 <light.omi1@gmail.com>
2026-08-11 18:53:20 +0100

    20260810 - using fish tts

--- va_bot.py
+++ va_bot.py
@@ -666,7 +666,7 @@ async def entrypoint(ctx: JobContext):
             inference.TTS(
                 model="fishaudio/s2.1-pro",
                 voice="v_a8NVrqPTCW4q",#"v_VeRxYTHdQqGg",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
-                extra_kwargs={"speed": 1.0, "temperature": 0.0, "latency": "low"},
+                extra_kwargs={"speed": 1.1, "temperature": 0.0, "latency": "low"},
             ),
             #build_azure_tts(),
             #build_elevenlabs_tts(),
