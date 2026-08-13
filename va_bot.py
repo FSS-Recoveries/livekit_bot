d89a134f31cf95373dc337a5942c0b555cc6e2fe
OlaOye24 <light.omi1@gmail.com>
2026-08-13 22:57:42 +0100

    20260813 - using inworld

--- va_bot.py
+++ va_bot.py
@@ -800,7 +800,7 @@ async def entrypoint(ctx: JobContext):
             inference.TTS(
                 model="fishaudio/s2.1-pro",
                 voice="v_VeRxYTHdQqGg",#"v_a8NVrqPTCW4q",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
-                extra_kwargs={"speed": 1.15, "temperature": 0.4, "latency": "normal"},
+                extra_kwargs={"speed": 1.15, "temperature": 0.1, "latency": "normal"},
             ),
 
             build_elevenlabs_tts(),
