159d74e925e30ab519d3cd9d189a016954ec277a
OlaOye24 <light.omi1@gmail.com>
2026-08-15 09:17:48 +0100

    20260813 - fish -> inworld -> elevenlabs

--- va_bot.py
+++ va_bot.py
@@ -798,17 +798,16 @@ async def entrypoint(ctx: JobContext):
     tts = agents_tts.FallbackAdapter(
         [
             inference.TTS(
-                model="inworld/inworld-tts-1.5-max",
-                voice="v_VeRxYTHdQqGg",#"v_a8NVrqPTCW4q",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
-                extra_kwargs={"speaking_rate": 1.35, "temperature": 0, "latency": "normal"},
-                ),
-            inference.TTS(
                 model="fishaudio/s2.1-pro",
                 voice="v_VeRxYTHdQqGg",#"v_a8NVrqPTCW4q",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
                 extra_kwargs={"speed": 1.25, "temperature": 0, "latency": "normal"},
             ),
 
-            
+            inference.TTS(
+                model="inworld/inworld-tts-1.5-max",
+                voice="v_VeRxYTHdQqGg",#"v_a8NVrqPTCW4q",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
+                extra_kwargs={"speaking_rate": 1.35, "temperature": 0, "latency": "normal"},
+                ),
                 
             build_elevenlabs_tts(),
 
