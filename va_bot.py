16429c16dcacd1221ae370d449869afea1557249
OlaOye24 <light.omi1@gmail.com>
2026-09-02 13:40:30 +0100

    fish model x2 before inworld

--- va_bot.py
+++ va_bot.py
@@ -960,13 +960,13 @@ async def entrypoint(ctx: JobContext):
             inference.TTS(
                 model="fishaudio/s2.1-pro",
                 voice="v_VeRxYTHdQqGg",#"v_a8NVrqPTCW4q",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
-                extra_kwargs={"speed": 1.35, "temperature": 0, "latency": "normal"},
+                extra_kwargs={"speed": 1.30, "temperature": 0, "latency": "normal"},
             ),
 
             inference.TTS(
                 model="fishaudio/s2-pro",
                 voice="v_VeRxYTHdQqGg",#"v_a8NVrqPTCW4q",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
-                extra_kwargs={"speed": 1.35, "temperature": 0, "latency": "normal"},
+                extra_kwargs={"speed": 1.30, "temperature": 0, "latency": "normal"},
             ),
 
             inference.TTS(
