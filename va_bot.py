56b33624ba47814b8e9e995acd25040a9f2b0618
OlaOye24 <light.omi1@gmail.com>
2026-09-21 16:06:53 +0100

    voice update

--- va_bot.py
+++ va_bot.py
@@ -959,13 +959,13 @@ async def entrypoint(ctx: JobContext):
         [
             inference.TTS(
                 model="fishaudio/s2.1-pro",
-                voice="v_ckr9NXBNDLXy",#"v_VeRxYTHdQqGg",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
+                voice="v_E8hZCwkMtY7y",#"v_VeRxYTHdQqGg",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
                 extra_kwargs={"speed": 1.25, "temperature": 0, "latency": "normal"},
             ),
 
             inference.TTS(
                 model="fishaudio/s2-pro",
-                voice="v_ckr9NXBNDLXy",#"v_ckr9NXBNDLXy",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
+                voice="v_E8hZCwkMtY7y",#"v_ckr9NXBNDLXy",#"v_XSvqo8UVEFYo","v_tkbNkcSD62zN",#"v_ebJJAf8QhLMs",
                 extra_kwargs={"speed": 1.25, "temperature": 0, "latency": "normal"},
             ),
 
